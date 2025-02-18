c3663e2bf62d42bc6b9c2010b9926880a054e908
Udara Samaratunge <samaratunge@gmail.com>
2025-02-18 16:35:12 +1300

    Add useful Comment for the Calculate method.

--- WinFormsApp1/Calculator.cs
+++ WinFormsApp1/Calculator.cs
@@ -8,6 +8,11 @@ namespace WinFormsApp1
 {
     internal class Calculator
     {
+        /**
+         * 
+         * This method calculate two numbers according to the operation
+         * 
+         */
         public double calculate(string operation, double no1, double no2)
         {
             if (operation == "+")
